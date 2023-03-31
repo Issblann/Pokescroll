b04fadb0662b3dd63b1b647d3089271ede74cf76
issblann <anaisabelpp@outlook.com>
2023-03-31 18:10:11 -0500

    proyecto terminado

---
+++ script.js
@@ -0,0 +1,19 @@
+gsap.to("#bg", {
+  scrollTrigger: {
+    scrub: 1,
+  },
+  scale: 1.5,
+});
+gsap.to("#ball", {
+  scrollTrigger: {
+    scrub: 1,
+  },
+  scale: 0.5,
+});
+
+gsap.to("#text", {
+  scrollTrigger: {
+    scrub: 1,
+  },
+  y: 500,
+});
